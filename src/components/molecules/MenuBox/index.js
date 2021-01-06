ed1aef00ed0c05e71ce7c8ff9f89db6e85f1bf02
stenlyadam <stenly.adam@unklab.ac.id>
2021-01-06 16:48:29 +0800

    fixing layout

--- src/components/molecules/MenuBox/index.js
+++ src/components/molecules/MenuBox/index.js
@@ -6,24 +6,26 @@ import {Button} from '../../atoms';
 const MenuBox = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Button
-        type="icon-with-label"
-        icon="icon-id-card"
-        label="ID Card"
-        onPress={() => navigation.navigate('StudentCard')}
-      />
-      <Button
-        type="icon-with-label"
-        icon="icon-schedule"
-        label="Schedule And Grade"
-        onPress={() => navigation.navigate('Schedule')}
-      />
-      <Button
-        type="icon-with-label"
-        icon="icon-account"
-        label="Account Balance"
-        onPress={() => navigation.navigate('AccountBalance')}
-      />
+      <View style={styles.boxContainer}>
+        <Button
+          type="icon-with-label"
+          icon="icon-id-card"
+          label="ID Card"
+          onPress={() => navigation.navigate('StudentCard')}
+        />
+        <Button
+          type="icon-with-label"
+          icon="icon-schedule"
+          label="Schedule And Grade"
+          onPress={() => navigation.navigate('Schedule')}
+        />
+        <Button
+          type="icon-with-label"
+          icon="icon-account"
+          label="Account Balance"
+          onPress={() => navigation.navigate('AccountBalance')}
+        />
+      </View>
       {/* <Button
         type="icon-with-label"
         icon="icon-achievement"
@@ -48,15 +50,8 @@ export default MenuBox;
 const styles = StyleSheet.create({
   container: {
     paddingVertical: 20,
-    width: '100%',
-    // height: 197,
-    height: 150,
     borderRadius: 10,
     backgroundColor: colors.white,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
     shadowColor: colors.black,
     shadowOffset: {
       width: 0,
@@ -65,5 +60,11 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.36,
     shadowRadius: 6.68,
     elevation: 11,
+  },
+  boxContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
